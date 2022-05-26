04b2018bb8ca75c3c4d7248049d2ed91b5fad4cb
Edwin Dijeont <91846586+EdwinDij@users.noreply.github.com>
2022-05-26 07:36:08 +0200

    Delete backback directory

--- backback/routes/comment.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const commentCtrl = require('../controllers/comment')
-
-router.get('/',commentCtrl.getComment);
-router.post('/', commentCtrl.createComment);
-
-module.exports = router;
